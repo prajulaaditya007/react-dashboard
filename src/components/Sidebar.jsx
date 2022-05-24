757be40f232c1e51af1236127a3aa379d9bfa4dd
prajulaaditya007 <en17el301100@medicaps.ac.in>
2022-05-24 10:09:10 +0530

    App edit - 1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import {SiShopware} from 'react-icons/si';
-import {MdOutlineCancel} from 'react-icons/md';
+import {MdOutlineCancel, MdShop2} from 'react-icons/md';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 
 import {links} from '../data/dummy';
@@ -26,7 +25,7 @@ const Sidebar = () => {
                     <div className="flex justify-between items-center">
                         <Link to="/" onClick={handleCloseSideBar}
                               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-                            <SiShopware/> <span>Shoppy</span>
+                            <MdShop2/> <span>ShopIt</span>
                         </Link>
                         <TooltipComponent content="Menu" position="BottomCenter">
                             <button
